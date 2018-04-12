a65ac1f0d7d32deeb7bd065297c92b8ca01b7ebe
Shivam Aditya <sa.shivam.aditya@gmail.com>
2018-04-12 19:31:11 +0530

    added a function to request autostart permission for other devices when given package name and activity name

--- android/src/main/java/com/reactlibrary/RNNotificationAndroidPermissionLibraryModule.java
+++ android/src/main/java/com/reactlibrary/RNNotificationAndroidPermissionLibraryModule.java
@@ -5,11 +5,14 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import java.lang.reflect.Method;
 
 public class RNNotificationAndroidPermissionLibraryModule extends ReactContextBaseJavaModule {
 
@@ -38,7 +41,6 @@ public class RNNotificationAndroidPermissionLibraryModule extends ReactContextBa
     @ReactMethod
     public void requestAutostartPermissionForXiaomi() {
         String manufacturer = "xiaomi";
-
         if (manufacturer.equalsIgnoreCase(android.os.Build.MANUFACTURER)) {
             //this will open auto start screen where user can enable permission for your app
             Intent intent = new Intent();
@@ -46,4 +48,55 @@ public class RNNotificationAndroidPermissionLibraryModule extends ReactContextBa
             getCurrentActivity().startActivity(intent);
         }
     }
+
+    @ReactMethod
+    public void requestAutostartPermissionForChineseDevices(String manufacturer, String security_app_packagename, String security_app_activityname) {
+        if (manufacturer.equalsIgnoreCase(android.os.Build.MANUFACTURER)) {
+            //this will open auto start screen where user can enable permission for your app
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName(security_app_packagename, security_app_activityname));
+            getCurrentActivity().startActivity(intent);
+        }
+    }
+
+//    @ReactMethod
+//    public void isAutostartPermissionRequired(Promise promise) {
+//        String manufacturer = "xiaomi";
+//
+//        try {
+//            Class<?> c = Class.forName("android.os.SystemProperties");
+//            if(c !=null){
+//                Method get = c.getMethod("get", String.class);
+//                String miui = (String) get.invoke(c, "ro.miui.ui.version.code");
+//
+//                Log.d("ReactNativeJS","miui is"+miui);
+//
+//                if(miui != ""){
+//                    promise.resolve(true);
+//                }
+//            }
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            String isMIUI = (Class.forName("android.os.SystemProperties") .getMethod("get", String::class.java) .invoke(null, "ro.miui.ui.version.code") as String? ?: "") != "";
+//
+//            Log.d("ReactNativeJS","isMIUI is"+isMIUI);
+//            if(isMIUI != ""){
+//                promise.resolve(true);
+//            }
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//        if (manufacturer.equalsIgnoreCase(android.os.Build.MANUFACTURER)) {
+//            //this will open auto start screen where user can enable permission for your app
+//            promise.resolve(true);
+//        }
+//
+//        promise.resolve(false);
+//    }
+
+
 }
